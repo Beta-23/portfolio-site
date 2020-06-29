60f733335c7e142759c573d684331299850f5885
Beta-23 <alfred.dominguez@gmail.com>
2020-06-28 23:12:12 -0700

    Update Header portfolio NavLink with correct condition

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
     <header>
         <h1>Portfolio</h1>
         <NavLink to="/" activeClassName="is-active" exact={ true }>Home</NavLink>  |
-        <NavLink to="/portfolio" activeClassName="is-active">Portfolio</NavLink>  |
+        <NavLink to="/portfolio" activeClassName="is-active" exact={ true }>Portfolio</NavLink>  |
         <NavLink to="/contact" activeClassName="is-active">Contact Me</NavLink>  |
     </header>
 );
