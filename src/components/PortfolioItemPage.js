cbcb079dfa95de8505d1bce0c5aa241cc7145ede
Beta-23 <alfred.dominguez@gmail.com>
2020-06-28 23:20:45 -0700

    Create a link back for portfolio item

--- src/components/PortfolioItemPage.js
+++ src/components/PortfolioItemPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Functional Components
 const PortfolioItem = (props) => (
   <div>
     <h1>Latest Work:</h1>
     <p>This page is for the item with the id of {props.match.params.id}</p>
+    <Link to="/portfolio">Back</Link>
   </div>
 );
 
