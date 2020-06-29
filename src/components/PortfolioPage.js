3d56067b2457ab38c98d91317d96c987cf2c45b9
Beta-23 <alfred.dominguez@gmail.com>
2020-06-28 23:07:28 -0700

    Change UI content for portfolioPage

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 // Functional Components
 const PortfolioPage = () => (
   <div>
-    <h1>My Work</h1>
-    <p>Checkout the stuff I've done:</p>
+    <h1>My Projects</h1>
+    <p>Checkout the work I've done:</p>
     <Link to="/portfolio/1">Item One</Link> <br />
     <Link to="/portfolio/2">Item Two</Link>
   </div>
