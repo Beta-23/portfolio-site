8ba1fbfb6b5f991aa00d6b8471e158429d43ee22
Beta-23 <alfred.dominguez@gmail.com>
2020-06-28 22:58:55 -0700

    Update broken link items to route to matching id path

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -1,16 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 // Functional Components
-const PortfolioPage = () => {
-    return (
-    <div>
-        <h1>My Work</h1>
-        <p>Checkout the latest projects:</p>
-        <p>Project One</p> |
-        <p>Project Two</p> |
-        <p>Project Three</p> 
-    </div>
-)};
+const PortfolioPage = () => (
+  <div>
+    <h1>My Work</h1>
+    <p>Checkout the stuff I've done:</p>
+    <Link to="/portfolio/1">Item One</Link> <br />
+    <Link to="/portfolio/2">Item Two</Link>
+  </div>
+);
 
 export default PortfolioPage;
